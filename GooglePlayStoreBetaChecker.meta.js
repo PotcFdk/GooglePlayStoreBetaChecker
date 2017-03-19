b4bbffcc0e3b4e20728cba31a731b90d8824ed40
PotcFdk <PotcFdk@gmx.net>
2017-03-19 16:08:47 +0100

    Hotfix: Add the missing https://play.google.com/apps include

--- GooglePlayStoreBetaChecker.meta.js
+++ GooglePlayStoreBetaChecker.meta.js
@@ -3,8 +3,9 @@
 // @name        Google Play Store Beta Checker
 // @namespace   https://github.com/PotcFdk/GooglePlayStoreBetaChecker
 // @description Checks if a beta program is available or active for any given app.
+// @include     https://play.google.com/apps
 // @include     https://play.google.com/store/apps/details*
-// @version     0.2.0
+// @version     0.2.1
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/GooglePlayStoreBetaChecker/master/GooglePlayStoreBetaChecker.user.js
 // @updateURL   https://raw.githubusercontent.com/PotcFdk/GooglePlayStoreBetaChecker/master/GooglePlayStoreBetaChecker.meta.js
